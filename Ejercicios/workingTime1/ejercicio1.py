7bf63fff784ba89614e29095be5e8bdac7e180ba
Bergerat Santiago <70027319+Yago030@users.noreply.github.com>
2024-02-07 15:11:41 -0300

    subiendo workin 2

--- Ejercicios/workingTime1/ejercicio1.py
+++ Ejercicios/workingTime1/ejercicio1.py
@@ -1,5 +1,8 @@
 numero = 0
-
-while numero <= 100:
+sumador = 0;
+while numero < 100:
     print("numero = " + str(numero))
     numero += 1
+    sumador +=numero;
+    
+print("La suma de todos los numeros es : " + str(sumador))
